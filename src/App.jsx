1672601e99349a455dc8b79e1c1f9a348dc8ee68
fxpby <fxpby@outlook.com>
2021-12-13 02:17:28 +0800

    feat: import zarm style on demand

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ import {
 import routes from './router'
 import {ConfigProvider} from 'zarm'
 import zhCN from 'zarm/lib/config-provider/locale/zh_CN'
-import 'zarm/dist/zarm.css'
 
 function App() {
   return <Router>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+import styleImport from 'vite-plugin-style-import'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    reactRefresh(),
+    styleImport(
+    {
+      libs: [
+        {
+          libraryName: 'zarm',
+          esModule: true,
+          resolveStyle: (name) => {
+            return `zarm/es/${name}/style/css`;
+          }
+        }
+      ]
+    }
+  )]
 })
