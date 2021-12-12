69efe9c8220cd6dece157e2c39e385cf0679d4ac
fxpby <fxpby@outlook.com>
2021-12-13 01:39:59 +0800

    feat: configure zarm

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,21 @@ import {
   Route
 } from 'react-router-dom'
 import routes from './router'
+import {ConfigProvider} from 'zarm'
+import zhCN from 'zarm/lib/config-provider/locale/zh_CN'
+import 'zarm/dist/zarm.css'
 
 function App() {
   return <Router>
-    <Routes>
-      {
-        routes.map(route => 
-          <Route exact key={route.path} path={route.path} element={<route.component />} />
-        )
-      }
-    </Routes>
+    <ConfigProvider primaryColor={'#007fff'} locale={zhCN}>
+      <Routes>
+        {
+          routes.map(route => 
+            <Route exact key={route.path} path={route.path} element={<route.component />} />
+          )
+        }
+      </Routes>
+    </ConfigProvider>
   </Router>
 }
 
