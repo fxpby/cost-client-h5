b1529508b34441c4611fc738ba95ff9c44a3becd
fxpby <fxpby@outlook.com>
2021-12-16 23:11:51 +0800

    feat: add CustomIcon

---
+++ src/components/CustomIcon/index.jsx
@@ -0,0 +1,3 @@
+import { Icon } from 'zarm'
+
+export default Icon.createFromIconfont('//at.alicdn.com/t/font_3024199_pfz2p7mr01n.js')
--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types'
 import { TabBar } from 'zarm';
 import { useNavigate, useLocation } from 'react-router-dom';
+import CustomIcon from '../CustomIcon'
 import s from './style.module.less';
 
 const NavBar = ({ showNav }) => {
@@ -19,14 +20,17 @@ const NavBar = ({ showNav }) => {
       <TabBar.Item
         itemKey="/"
         title="账单"
+        icon={<CustomIcon type="icon-accountbook" />}
       />
       <TabBar.Item
         itemKey="/data"
         title="统计"
+        icon={<CustomIcon type="icon-piechart" />}
       />
       <TabBar.Item
         itemKey="/user"
         title="我的"
+        icon={<CustomIcon type="icon-user" />}
       />
     </TabBar>
   )
