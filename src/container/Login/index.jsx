623fc9d8353c59f534ad5e4afd0f6dccd8877e88
fxpby <fxpby@outlook.com>
2021-12-31 15:13:16 +0800

    feat: update register and login logic

--- src/container/Login/index.jsx
+++ src/container/Login/index.jsx
@@ -3,6 +3,7 @@ import { Cell, Input, Button, Checkbox, Toast } from 'zarm'
 import CustomIcon from '@/components/CustomIcon'
 import Captcha from 'react-captcha-code'
 import { post } from '@/utils'
+import cx from 'classnames'
 
 import s from './style.module.less'
 
@@ -12,46 +13,64 @@ const Login = () => {
   const [verify, setVerify] = useState('')
   const [captcha, setCaptcha] = useState('')
 
+  // 登录/注册类型切换
+  const [pageType, setPageType] = useState('login')
+
   const handleChange = useCallback((captcha) => {
     setCaptcha(captcha)
   }, [])
 
   const onSubmit = async () => {
-    if (!username) {
-      Toast.show('请输入账号')
-      return
-    }
-
-    if (!password) {
-      Toast.show('请输入密码')
-      return
-    }
-
-    if (!verify) {
-      Toast.show('请输入验证码')
-      return
-    }
-
-    if (verify !== captcha) {
-      Toast.show('验证码错误')
-      return
-    }
-
-    try {
-      const {data} = await post('/api/user/register', {
-        username,
-        password
-      })
-      Toast.show('注册成功🎈')
-    } catch (e) {
-      Toast.show(e && e.msg)
+    if (pageType === 'login') {
+      try {
+        const { data } = await post('/api/user/login', {
+          username,
+          password
+        })
+        localStorage.setItem('token', data.token)
+      } catch (e) {
+        Toast.show(e && e.msg)
+        return
+      }
+    } else {
+      if (!username) {
+        Toast.show('请输入账号')
+        return
+      }
+  
+      if (!password) {
+        Toast.show('请输入密码')
+        return
+      }
+  
+      if (!verify) {
+        Toast.show('请输入验证码')
+        return
+      }
+  
+      if (verify !== captcha) {
+        Toast.show('验证码错误')
+        return
+      }
+  
+      try {
+        const {data} = await post('/api/user/register', {
+          username,
+          password
+        })
+        Toast.show('注册成功🎈')
+        setPageType('login')
+      } catch (e) {
+        Toast.show(e && e.msg)
+      }
     }
   }
 
   return <div className={s.auth}>
     <div className={s.head}></div>
     <div className={s.tab}>
-      <span>注册</span>
+      <span className={cx({ [s.active]: pageType === 'login' })} onClick={() => setPageType('login')}>登录</span>
+      <span className={cx({ [s.active]: pageType === 'register' })} onClick={() => setPageType('register')}>注册</span>
     </div>
     <div className={s.form}>
       <Cell icon={<CustomIcon type="icon-user"/>}>
@@ -70,24 +89,32 @@ const Login = () => {
           onChange={(value) => setPassword(value)}
         />
       </Cell>
-      <Cell icon={<CustomIcon type="icon-password"/>}>
-        <Input
-          clearable
-          type="text"
-          placeholder="请输入验证码"
-          onChange={(value) => setVerify(value)}
-        />
-        <Captcha charNum={4} onChange={handleChange} />
-      </Cell>
+      {
+        pageType === 'register' 
+        ? <Cell icon={<CustomIcon type="icon-password"/>}>
+            <Input
+              clearable
+              type="text"
+              placeholder="请输入验证码"
+              onChange={(value) => setVerify(value)}
+            />
+            <Captcha charNum={4} onChange={handleChange} />
+          </Cell>
+        : null
+      }
     </div>
     <div className={s.operation}>
-      <div className={s.agree}>
-        <Checkbox />
-        <label className="text-light">阅读并同意
-          <a>阅读条款啦啦啦</a>
-        </label>
-      </div>
-      <Button block theme="primary" onClick={onSubmit}>注册</Button>
+      {
+        pageType === 'register'
+        ? <div className={s.agree}>
+            <Checkbox />
+            <label className="text-light">阅读并同意
+              <a>阅读条款啦啦啦</a>
+            </label>
+          </div>
+        : null
+      }
+      <Button block theme="primary" onClick={onSubmit}>{pageType === 'register' ? '注册' : '登录'}</Button>
     </div>
   </div>
 }
